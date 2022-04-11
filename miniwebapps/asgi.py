89f77192190047323cff80f9be3363c199866722
Ali Ahmed <sibkhan131@gmail.com>
2022-04-11 19:17:22 +0100

    Changed main project name

--- miniwebapps/asgi.py
+++ miniwebapps/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for qrCodeMaker project.
+ASGI config for miniwebapps project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'qrCodeMaker.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'miniwebapps.settings')
 
 application = get_asgi_application()
